71b6a83735529a4101a1d1dfc67d256d8f539e5c
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-09-24 11:18:28 +0300

    miniNav

--- js/index.js
+++ js/index.js
@@ -160,12 +160,20 @@ $(document).ready(function () {
 
     var $sections = $('#section_about, #section_overview, #section_design, #section_sitemap, #section_desktop, #section_mobile, #section_highlights');
     var $navLinks = $('.miniNav--nav_content-stage a');
+    var $nav = $('.miniNav--nav');
+    var navOffsetTop = $nav.offset().top;
 
     $(window).on('scroll', function () {
         var scrollTop = $(window).scrollTop();
 
+        if (scrollTop > navOffsetTop) {
+            $nav.addClass('fixed');
+        } else {
+            $nav.removeClass('fixed');
+        }
+
         $sections.each(function () {
-            var $section = $(this); 
+            var $section = $(this);
             var sectionTop = $section.offset().top;
             var sectionBottom = sectionTop + $section.outerHeight();
 
@@ -173,7 +181,7 @@ $(document).ready(function () {
                 var id = $section.attr('id');
                 var $currentLink = $navLinks.filter('[href="#' + id + '"]');
 
-                if (!$currentLink.hasClass('active')) {  
+                if (!$currentLink.hasClass('active')) {
                     $navLinks.removeClass('active');
                     $currentLink.addClass('active');
                 }
